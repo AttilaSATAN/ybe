abfb90273f92645600351d4261a1dd42a2674c61
Attila SATAN <attila@lupuseum.com>
2014-09-22 09:43:42 +0300

    build

--- YBE/config/env/production.js
+++ YBE/config/env/production.js
@@ -14,7 +14,10 @@ module.exports = {
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+                'public/lib/lodash/dist/lodash.min.js',
+				'//maps.googleapis.com/maps/api/js?sensor=false',
+				'public/lib/angular-google-maps/dist/angular-google-maps.js',
 			]
 		},
 		css: 'public/dist/application.min.css',
