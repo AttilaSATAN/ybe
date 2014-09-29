07dbbe2b499297689acddaa369ef82ecf5ccf865
Attila SATAN <attila@lupuseum.com>
2014-09-29 03:22:54 +0300

    resimler

---
+++ YBE/public/modules/core/controllers/core.client.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+angular.module('core').controller('CoreController', ['$scope','$rootScope',
+    function ($scope, $rootScope) {
+        /*
+        * 
+        */
+        $rootScope.layoutState = 'default';
+    }
+]);
