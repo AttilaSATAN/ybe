7ec02b74a3e3493833063547ef90e5ed9335dfc9
Attila SATAN <attila@lupuseum.com>
2014-10-26 02:08:45 +0300

    little init

--- YBE/app/routes/core.server.routes.js
+++ YBE/app/routes/core.server.routes.js
@@ -4,8 +4,5 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core');
 	//app.route('/').get(core.index);
-	app.route('/ask').get(function (req, res) {
-		res.send('oldu');
-	});
 	app.route('*').get(core.index);
 };
